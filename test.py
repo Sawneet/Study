0714929c9096ccee51ef4dd7818bf8f57e4a8c56
Sawneet <106180781+Sawneet@users.noreply.github.com>
2023-06-17 22:58:41 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,33 @@
+'''
+i=1
+while i<= 1000:
+  print(i)
+  i=i+1
+'''
+'''
+i = 1
+while i<=20:
+  print(i)
+  i=i+2
+'''
+'''
+i=1
+while i<=10:
+  print(i*10)
+  i= i+1
+'''
+'''
+price_of_house = 1000000
+has_good_credit = True
+if has_good_credit:
+  payment = price_of_house * 0.1
+  
+else:
+  payment = price_of_house * 0.2
+
+print("down payment is" + str(payment))
+'''
+i = 1
+while i <= 10:
+  print(i*6)
+  i = i+1
